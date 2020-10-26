e718da3eb31e8a2270a4cbfb02876cdde4309cce
Mirko Guerra <mirkoguerra93@gmail.com>
2020-10-26 13:45:53 +0100

    HTML
    La visualizzazione è ora disponibile non solo in console, ma anche in HTML, con lista ordinata degli elementi

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 
+var lista = document.getElementById("lista");
+
 for (var i = 1; i <= 100; i++){
   if (i % 3 == 0 && i % 5 == 0){
+    lista.innerHTML += "<li>" + "FizzBuzz" + "</li>";
     console.log("FizzBuzz");
   } else if (i % 3 == 0) {
+    lista.innerHTML += "<li>" + "Fizz" + "</li>";
     console.log("Fizz");
   } else if (i % 5 == 0) {
+    lista.innerHTML += "<li>" + "Buzz" + "</li>";
     console.log("Buzz");
   } else {
+    lista.innerHTML += "<li>" + i + "</li>";
     console.log(i);
   }
 }
